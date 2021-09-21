0ce9df61e2d08077c9ac5f41bf8e81bc41552357
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-21 09:57:41 -0700

    artist search working

--- src/components/ArtistList.jsx
+++ src/components/ArtistList.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Artist from './Artist';
 
-const ArtistList = ({ artists }) => (
+const ArtistList = ({ artists }) => {
+  console.log(artists, 'artists')
+  return (
     <ul aria-label="artists">
-        {artists.map(({ id, name}) => (
-            <li key={id}> 
-                <Artist
-                 id={id}
-                 name={name}
-                />
-            </li>
-        ))}
+      {artists.map(({ id, name }) => (
+        <li key={id}>
+          <Artist id={id} name={name} />
+        </li>
+      ))}
     </ul>
-);
+  );
+};
 
 ArtistList.propTypes = {
-    artists: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name:PropTypes.string.isRequired,
-        }).isRequired
-    )
+  artists: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }).isRequired
+  ),
 };
 
 export default ArtistList;
