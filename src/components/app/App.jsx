abab9f8b9936a3466b6bbd8df62f2cf5d74a33d6
BenFrancka <benfrancka@gmail.com>
2021-09-21 10:36:37 -0700

    fuxing brackets

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import ArtistSearch from '../../containers/ArtistSearch';
 
 export default function App() {
   return (
-    <>
-      <ArtistSearch />
-    </>
+    <Switch>
+      <Route exact path="/">
+        <ArtistSearch />
+      </Route>
+      <Route exact path="/realeases/:id">
+
+      </Route>
+      <Route exact path="/recordings/:id">
+
+      </Route>
+      <Route exact path="/songs/:id">
+
+      </Route>
+    </Switch>
   );
 }
